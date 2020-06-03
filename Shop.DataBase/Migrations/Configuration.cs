b939aa0d031b3fca9d806d3ad44972580e561a0c
nitochka-ac <66315752+nitochka-ac@users.noreply.github.com>
2020-06-03 12:54:07 +0300

    368
    zxcb

---
+++ Shop.DataBase/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace Shop.DataBase.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Shop.DataBase.SHContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Shop.DataBase.SHContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
---
+++ Shop.DataBase/SHContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using Shop.Entity;
+
+namespace Shop.DataBase
+{
+    class SHContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public SHContext() : base("ShopConnection")
+        {
+
+        }
+    }
+}
